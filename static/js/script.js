a44f3f4eb5019a9fa7f3553e3340e1fadc084224
mc227 <markcostales.mark@gmail.com>
2022-04-15 13:49:37 -0400

    works so far

--- static/js/script.js
+++ static/js/script.js
@@ -143,3 +143,50 @@ function randomColors() {
         all_buttons[i].classList.add(choices[randomNumber])
     }
 }
+
+// Challenge 5: Blackjack
+let blackjackGame = {
+    'you': {'scoreSpan' : '#your-blackjack-result', 'div':'#your-box', 'score':0},
+    'dealer': {'scoreSpan' : '#dealer-blackjack-result', 'div':'#dealer-box', 'score':0},
+    'cards': ['2','3','4','5','6','7','8','9','10','K','J','Q','A'],
+};
+
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+
+const hitSound =  new Audio('static/sounds/sounds_swish.m4a')
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+function blackjackHit() {
+    let card = randomCard();
+    showCard(card, YOU);
+    // showCard(DEALER);
+}
+
+function randomCard() {
+    let randomIndex = Math.floor(Math.random() * 13)
+    return blackjackGame.cards[randomIndex]
+}
+
+function showCard(card, activePlayer) {
+    let cardImage = document.createElement('img')
+    cardImage.src = `static/images/${card}.png`
+    document.querySelector(activePlayer['div']).appendChild(cardImage)
+    hitSound.play();
+}
+
+function blackjackDeal() {
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+    
+    for (let i = 0; i < yourImages.length; i++) {
+        yourImages[i].remove();
+    }
+
+    for (let i = 0; i < dealerImages.length; i++) {
+        dealerImages[i].remove();
+    }
+}
+
